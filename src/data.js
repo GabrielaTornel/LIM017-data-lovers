d3f15a9c637feb34f0c9c2619af481789e650de2
gabriela tornel <tornelgabriela@gmail.com>
2022-04-01 20:14:25 -0500

    botones pagina 3-4 conectados todos

--- src/data.js
+++ src/data.js
@@ -1,13 +1,56 @@
-// estas funciones son de ejemplo
+export const sortMovies = (dataFilms, sortBy, sortOrder) => {
+  let orderSelectF = [];
+        /* lo siguiente ordena según el titulo, dividiendose en ascendente(true) y descendente(false) */
+        if (sortBy === "title") {
+          // ordena de forma ascendente
+          if (sortOrder === "A-Z"){
+              //aplicamos slice para que no modifique el array original y asi tenerlos sin orden por si quiere sacar el orden.  El sort hace el trabajo de filtrar ascendente o descendente
+              orderSelectF= dataFilms.sort(function(x, y) {
+                if (x.title > y.title) return 1;
+                if (y.title === x.title) return 0;
+                return -1;
+              });
+          } else {
+            orderSelectF = dataFilms.sort(function(x, y) { // ordena de forma descendente
+              //aplicamos slice para que no modifique el array original y asi tenerlos sin orden por si quiere sacar el orden. El sort hace el trabajo de filtrar ascendente o descendente
+              if (y.title > x.title) return 1;
+              if (y.title === x.title) return 0;
+              return -1;
+            });
+          }
+        } else if (sortBy === "release_date") {
+          // ordena de forma ascendente fechas lanzamiento
+        if (sortOrder === "sortDateAsc") {
+          orderSelectF = dataFilms.sort(function(x, y) {
+            if (x.release_date > y.release_date) return 1;
+        if (x.release_date === y.release_date) return 0;
+        return -1;
+      });
+    }  else {
+      orderSelectF = dataFilms.sort(function(x , y) {
+            if (y.release_date > x.release_date) return 1;
+        if (y.release_date === x.release_date) return 0;
+        return -1;
+      })
+    }
+    }
+return orderSelectF;
+}
 
-export const dataMovies = () => {
-  //informacion de data
 
-
-
-  return 'dataMovies';
+export const filterByGender = function(dataPeople, genderData){
+const filterData= dataPeople.filter(element => element.gender === genderData);
+return filterData;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export const  computeStats = (dataFilms, nameDirector) =>{
+  //constante que guarde funcion de filtro de directores 
+  const moviesByDirector = (dataFilms.filter(y => y.director === nameDirector));
+  // constante guarda el numero de directores
+  const count = moviesByDirector.length;
+  //constante que realiza la operacion matemtica,regla de tres
+    const percent = Math.round((count / dataFilms.length)*100);
+  return percent;
 };
+
+
--- src/main.js
+++ src/main.js
@@ -1,18 +1,39 @@
-import {dataMovies} from './data.js';
-import data from './data/ghibli/ghibli.js';
-console.log (data);
-console.log(dataMovies);
+import {sortMovies , filterByGender, computeStats} from "./data.js";
+import data from "./data/ghibli/ghibli.js";
+console.log (computeStats);
 
+
+//console.log (sortMovies)
 
 let bttnMoreInf = document.getElementById("bttnMoreInf");
 bttnMoreInf.addEventListener("click",()=>{
+const pageOne=document.getElementById("pageOne");
+pageOne.style.display="none";
+const pageTwo=document.getElementById("pageTwo");
+pageTwo.style.display="";
+})
+let bttnMoreInf2 = document.getElementById("bttnMoreInf2");
+bttnMoreInf2.addEventListener("click",()=>{
   const pageOne=document.getElementById("pageOne");
   pageOne.style.display="none";
   const pageTwo=document.getElementById("pageTwo");
 pageTwo.style.display="";
+const pageThree=document.getElementById("pageThree");
+pageThree.style.display="none";
+const pageFour=document.getElementById("pageFour");
+pageFour.style.display="none";
 })
-
-
+let bttnMoreInf3 = document.getElementById("bttnMoreInf3");
+bttnMoreInf3.addEventListener("click",()=>{
+  const pageOne=document.getElementById("pageOne");
+  pageOne.style.display="none";
+  const pageTwo=document.getElementById("pageTwo");
+pageTwo.style.display="";
+const pageThree=document.getElementById("pageThree");
+pageThree.style.display="none";
+const pageFour=document.getElementById("pageFour");
+pageFour.style.display="none";
+})
 let backIntro = document.getElementById("backIntro");
 backIntro.addEventListener("click",()=>{
   const pageOne=document.getElementById("pageOne");
@@ -21,34 +42,278 @@ backIntro.addEventListener("click",()=>{
 pageTwo.style.display="none";
 })
 
-// import data from './data/rickandmorty/rickandmorty.js';
+let btnPeople = document.getElementById("btnPeople");
+btnPeople.addEventListener("click",()=>{
+  const pageTwo=document.getElementById("pageTwo");
+pageTwo.style.display="none";
+const pageThree=document.getElementById("pageThree");
+pageThree.style.display="";
+const pageFour=document.getElementById("pageFour");
+pageFour.style.display="none";
+})
+let btnPeople2 = document.getElementById("btnPeople2");
+btnPeople2.addEventListener("click",()=>{
+  const pageOne=document.getElementById("pageOne");
+  pageOne.style.display="none";
+  const pageTwo=document.getElementById("pageTwo");
+pageTwo.style.display="none";
+const pageThree=document.getElementById("pageThree");
+pageThree.style.display="";
+})
 
-let myArray = data.films;
-//let myList = "";
+let backIntro2 = document.getElementById("backIntro2");
+backIntro2.addEventListener("click",()=>{
+  const pageOne=document.getElementById("pageOne");
+  pageOne.style.display="";
+  const pageTwo=document.getElementById("pageTwo");
+pageTwo.style.display="none";
+const pageThree=document.getElementById("pageThree");
+pageThree.style.display="none";
+})
 
-myArray.forEach((displayAllData) => {
-  document.getElementById("filmsInfo").innerHTML += `
-  <div class="card">
-  <div class="filmsTitle"> Title: ${displayAllData.title}</div>
-  <br>
-  <div class="filmsDescription"> Description: ${displayAllData.description}</div>
-  <img class="poster" src="${displayAllData.poster}">
-   <br>
+let btnStatistics = document.getElementById("btnStatistics");
+btnStatistics.addEventListener("click",()=>{
+  const pageFour=document.getElementById("pageFour");
+  pageFour.style.display="";
+  const pageTwo=document.getElementById("pageTwo");
+pageTwo.style.display="none";
+const pageThree=document.getElementById("pageThree");
+pageThree.style.display="none";
+const pageOne=document.getElementById("pageOne");
+pageOne.style.display="none";
+})
+let backIntro3 = document.getElementById("backIntro3");
+backIntro3.addEventListener("click",()=>{
+  const pageFour=document.getElementById("pageFour");
+  pageFour.style.display="none";
+  const pageTwo=document.getElementById("pageTwo");
+pageTwo.style.display="none";
+const pageThree=document.getElementById("pageThree");
+pageThree.style.display="none";
+const pageOne=document.getElementById("pageOne");
+pageOne.style.display="";
+})
+let btnStatistics2 = document.getElementById("btnStatistics2");
+btnStatistics2.addEventListener("click",()=>{
+  const pageFour=document.getElementById("pageFour");
+  pageFour.style.display="";
+  const pageTwo=document.getElementById("pageTwo");
+pageTwo.style.display="none";
+const pageThree=document.getElementById("pageThree");
+pageThree.style.display="none";
+const pageOne=document.getElementById("pageOne");
+pageOne.style.display="none";
+})
+
+
+
+
+let filmsData = data.films;
+
+//nombrando nuestra lista recorrida
+const totalDataFilms = (listData) =>{
+  let resultMovies = "";
+//recorriendo nuestro array
+listData.forEach((filmsData) => {
+  const dataCard = `
+  <div class="cardContainer">
+  <div class="cardInner">
+  <div class="cardFront">
+  <img class="poster" src="${filmsData.poster}">
+  <div class="filmsTitle"> ${filmsData.title}</div>
+  <div class="filmsYear"> Year: ${filmsData.release_date} <span>  ⭐ ${filmsData.rt_score}</span>
   </div>
-  `
-    })
+  </div>
+  <div class="cardBack">
+  <div class="cardTextBack">
+  <strong> Director: </strong>  ${filmsData.director}
+  <br>
+  <strong> Description:</strong> ${filmsData.description}
+  </div>
+  </div>
+  </div>
+  </div>
+  `;
+  resultMovies += dataCard ;
+    });
+    document.getElementById("filmsInfo").innerHTML= resultMovies;
+  }
+  totalDataFilms(filmsData);
+ /* console.log(sortMovies(myArray, "title", "A-Z"))
+  console.log(sortMovies(myArray, "release_date" , "sortDateAsc"))*/
+ document.getElementById("selectSortAZ").addEventListener("change", (e) => {
+  const selectedIndex = e.currentTarget.value;
+  //debugger
+  // sortBy.options[sortBy.selectedIndex].value;
+  if (selectedIndex == "A-Z") {
+    totalDataFilms(sortMovies(filmsData, "title", "A-Z"));
+  }if (selectedIndex === "Z-A") {
+    totalDataFilms(sortMovies(filmsData, "title", "Z-A"));
+  }if (selectedIndex === "sortDateAsc") {
+    totalDataFilms(sortMovies(filmsData, "release_date", "sortDateAsc"));
+  }if (selectedIndex === "sortDateDes") {
+    totalDataFilms(sortMovies(filmsData, "release_date", "sortDateDes"));
+  }else{
+ return totalDataFilms(filmsData);
+  }
+});
+
+//traemos la data "gender" del objeto e imprimimos en interfaz
+  //concatenamos arrays para acceder a nuestros personajes del objeto
+let peopleValue=[];
+  for (let element of filmsData) {
+ peopleValue.push(element.people);
+  }
+let listOfPeople = peopleValue[0];
+ for (let i=1; i<peopleValue.length; i++){
+   listOfPeople = listOfPeople.concat(peopleValue[i]);
+ }
+ //console.log(listOfPeople); //Este array contiene todos los personajes.
+ 
+ let peopleGender=[];
+  for (let element of listOfPeople) {
+ peopleGender.push(element.gender);
+  }
+/*const genderArr= new Set(peopleGender);
+ let result = [...genderArr];
+ console.log(result);
+ /*console.log(filterByGender(filmsData, peopleGender.Female)); */
+
+const totalPeople = (listData) =>{
+  let resultPeople = "";
+//recorriendo nuestro array
+listData.forEach((listOfPeople) => {
+  const dataPeopleObj = `
+  <div class="cardContainerPeople">
+  <div class="cardInner">
+  <div class="cardFront">
+  <img class="posterPeople" src="${listOfPeople.img}">
+  <div class="filmsPeopleName">
+  Name: ${listOfPeople.name}
+  <br>
+  Gender: ${listOfPeople.gender}
+  <br>
+  Specie:  ${listOfPeople.specie}
+  <br>
+  Eye color: ${listOfPeople.eye_color}
+  </div>
+  </div>
+  </div>
+  </div>
+  `;
+  resultPeople += dataPeopleObj ;
+    });
+  document.getElementById("filmsPeopleCard").innerHTML= resultPeople;
+  }
+  totalPeople(listOfPeople);
+
+
+//Aplicando evento al FILTER
+document.getElementById("selectGender").addEventListener("change", (e) => {
+  const selectedFilter = e.currentTarget.value;
+  //debugger
+  // sortBy.options[sortBy.selectedIndex].value;
+  if (selectedFilter == "002") {
+   return totalPeople(filterByGender(listOfPeople, "Female"));
+  }else if(selectedFilter == "001") {
+    return totalPeople(filterByGender(listOfPeople, "Male"));
+  } else if(selectedFilter == "003") {
+    return totalPeople(filterByGender(listOfPeople, "NA"));
+  }else {
+      return totalPeople(filterByGender(listOfPeople, "Unknown (Possible Male)"));
+   }
+  });
+ /*
+let resultPorcent ="" 
+let directorList=[];
+for (let element of filmsData) {
+directorList.push(element.director);
+}
+
+console.log(filterByGender(listOfPeople,"Unknown (Possible Male)"))*/
+
+
+//Estadísticas
+/* let director = filmsData.map((x) => x.director);
+director = director.filter((item, i) =>{
+  return director.indexOf(item) === i;
+}) */
+//aca el intento de gabriela xD
+let directorList=[];
+for (let element of filmsData) {
+directorList.push(element.director);
+}
+
+// const myDirectorChart= document.getElementById("filmsStadistics").getContext("2d");
+let porcentaje =[];
+for (let element of directorList){
+  porcentaje.push((computeStats(filmsData, element)));
+}
+
+
+let directorPercent = `${directorList} ${porcentaje} `;
+
+
+console.log(directorPercent)
+
+
+//const myDirectorChart= document.getElementById("filmsStadistics").getContext("2d");
+
+/* let directorPercent = directorList + porcentaje;
+
+console.log(directorPercent);*/ 
 
 
 
+var result = [],
+    i, l = Math.min(directorList.length, porcentaje.length);
+    
+for (i = 0; i < l; i++) {
+    result.push(...directorList, ...porcentaje);
+}
+// result.push(...directorList.slice(l), ...porcentaje.slice(l));
 
 
+const directorArr= new Set(result);
+ let resultFinal = [...directorArr];
 
+ console.log(resultFinal);
 
-
-  
-
-//'<li>'+index.people+' '+index.people+'</li>'
-/*console.log(data);
-console.log(dataMovies)
-*/
-
+ /* function getPercent(a){
+  new Chart (a, {
+            type:'pie',
+            data: {
+                labels: director,
+                datasets: [{
+                  data: porcentaje,
+                  backgroundColor: [
+                  'rgb(255, 171, 193, 0.9)',
+                  'rgb(156, 170, 242, 0.9)',
+                  'rgb(255, 202, 203, 0.9)',
+                  'rgb(149, 203, 255, 0.9)',
+                  'rgb(255, 244, 209, 0.9)',
+                  'rgb(180, 229, 255, 0.9)'
+                    ],
+                },
+              ]},
+              options: {
+                responsive: true,
+                plugins: {
+                  legend: {
+                    position: 'top',
+                  },
+                  title: {
+                    position: 'bottom',
+                    display: true,
+                    text: `Isao Takahata: 25%
+                    Hayao Miyazaki: 45%
+                    Gorō Miyazaki:10%
+                    Hiromasa Yonebayashi:10%
+                    Hiroyuki Morita: 5%
+                    Yoshifumi Kondō: 5%`
+                  }
+                }
+              },
+        })
+      }
+    getPercent(mySecondChart); */
